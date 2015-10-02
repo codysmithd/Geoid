7ffccc8843811b59ede3fa00a70daf0ef9df1ecd
Cody Smith <codysmithd@gmail.com>
2015-10-02 12:51:11 -0400

    New initial Commit

---
+++ Geoid/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Geoid
+//
+//  Created by Cody Smith on 10/2/15.
+//  Copyright © 2015 Cody Smith. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Geoid/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Geoid
+//
+//  Created by Cody Smith on 10/2/15.
+//  Copyright © 2015 Cody Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
